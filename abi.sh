2c8ce24cbd8d659e55a9bc435054797b7076c0ae
Konstantin Belousov <kib@freebsd.org>
2017-01-30 17:56:51 +0200

    Rename comparision results for compat32 libs, to not override native results.

--- abi.sh
+++ abi.sh
@@ -14,26 +14,29 @@ mkdir -p tmp
 rm -rf compat_reports tmp/*
 troubles=
 for obj in $objs; do
-#	    -public-headers ${PRISTINE_BASE}/tmp/usr/include \
-	libname=$(basename ${obj} | sed 's/\.full//')
-	for x in $exclude_list; do
-		if [ $x \= $libname ] ; then
-			continue 2
-		fi
-	done
-	changed_obj=${CHANGED_BASE}/${obj}
-	pristine_obj=$(echo ${changed_obj} | sed s/ino64/ino64_master/g)
-	abi-dumper.pl ${pristine_obj} \
-	    -lver pristine \
-	    -o tmp/${libname}-pristine.dump
-	abi-dumper.pl ${changed_obj} \
-	    -lver ino64 \
-	    -o tmp/${libname}-ino64.dump
-	if abi-compliance-checker.pl -l ${libname} \
-	    -old tmp/${libname}-pristine.dump \
-	    -new tmp/${libname}-ino64.dump ; then : ; else
-		troubles="${troubles} ${libname}"
+#	    -public-headers ${PRISTINE_BASE}/tmp/usr/include
+    if echo $obj | grep world32 ; then
+	suffix=32
+    else
+	suffix=""
+    fi
+    libname=$(basename ${obj}${suffix} | sed 's/\.full//')
+    for x in $exclude_list; do
+	if [ $x \= $libname ] ; then
+		continue 2
 	fi
+    done
+    changed_obj=${CHANGED_BASE}/${obj}
+    pristine_obj=$(echo ${changed_obj} | sed s/ino64/ino64_master/g)
+    abi-dumper.pl ${pristine_obj} \
+      -lver pristine -o tmp/${libname}-pristine.dump
+    abi-dumper.pl ${changed_obj} \
+      -lver ino64 -o tmp/${libname}-ino64.dump
+    if abi-compliance-checker.pl -l ${libname} \
+      -old tmp/${libname}-pristine.dump \
+      -new tmp/${libname}-ino64.dump ; then : ; else
+	troubles="${troubles} ${libname}"
+    fi
 done
 
 echo "Troublesome libraries: " ${troubles}
